a9c95190040d3168508e6a7f7cfabae00aa57bd5
ecobost <ecobos21@hotmail.com>
2016-07-23 18:08:16 -0500

    Rephrase it as a script

--- code/generate_responses.py
+++ code/generate_responses.py
@@ -15,60 +15,63 @@ import numpy as np
 import matplotlib.pyplot as plt
 import nipy.modalities.fmri.hemodynamic_models as hm
 
-# Read BOLD data file
-voxel_file = h5py.File('VoxelResponses_subject1.mat', 'r')
-rois = voxel_file['roi']
-bold = voxel_file['rt'] # 'rv' for test responses
+def main():
+	"""Main function. Executed if run as script."""
+	# Read BOLD data file
+	voxel_file = h5py.File('VoxelResponses_subject1.mat', 'r')
+	rois = voxel_file['roi']
+	bold = voxel_file['rt'] # 'rv' for test responses
 
-# Extract ROIs (later used as mask)
-brain = np.zeros([18, 64, 64])
-i = 1
-for roi in rois:
-	brain = brain + 8*i*np.array(rois[roi])
-	i += 8
+	# Extract ROIs (later used as mask)
+	brain = np.zeros([18, 64, 64])
+	i = 1
+	for roi in rois:
+		brain = brain + 8*i*np.array(rois[roi])
+		i += 8
 
-# Save ROIs as images (to check against mean EPI and visualization)
-for i in range(18):
-	plt.imsave('coronal_' + str(i+1) + '.png', brain[i,...])
+	# Save ROIs as images (to check against mean EPI and visualization)
+	for i in range(18):
+		plt.imsave('coronal_' + str(i+1) + '.png', brain[i,...])
 
-# Create mean EPI
-bold = np.array(bold).transpose()
-epi_mean = bold.mean(axis = 0)
-epi_mean = epi_mean.reshape([18, 64, 64])
-for i in range(18):
-	plt.imsave('mean_' + str(i+1) + '.png', epi_mean[i,...])
+	# Create mean EPI
+	bold = np.array(bold).transpose()
+	epi_mean = bold.mean(axis = 0)
+	epi_mean = epi_mean.reshape([18, 64, 64])
+	for i in range(18):
+		plt.imsave('mean_' + str(i+1) + '.png', epi_mean[i,...])
 
-# Apply ROI mask
-roi_mask = (brain > 0).flatten()
-bold = bold[:, roi_mask]
+	# Apply ROI mask
+	roi_mask = (brain > 0).flatten()
+	bold = bold[:, roi_mask]
 
-# Clean data (delete columns with any nans)
-nan_cols = np.isnan(bold).any(axis=0)
-nan_mask = np.logical_not(nan_cols) 
-bold = bold[:, nan_mask] # 8982 remaining voxels for S1
-# Tested: Same voxels are dropped for test responses, i.e., masks are the same
+	# Clean data (delete columns with any nans)
+	nan_cols = np.isnan(bold).any(axis=0)
+	nan_mask = np.logical_not(nan_cols) 
+	bold = bold[:, nan_mask] # 8982 remaining voxels for S1
+	# Tested: Same voxels are dropped for test responses, i.e., masks are the same
 
-# Save as BOLD
-bold_file = h5py.File('bold.h5', 'w')
-bold_file.create_dataset('responses', data=bold)
-bold_file.close()
+	# Save as BOLD
+	bold_file = h5py.File('bold.h5', 'w')
+	bold_file.create_dataset('responses', data=bold)
+	bold_file.close()
 
-# Save as BOLD with different delays (from 4-8 secs)
-for delay in [4, 5, 6, 7, 8]:
-	delay_bold = np.full(bold.shape, np.nan, dtype = 'float32')
-	delay_bold[delay:, :] = bold[:-delay, :]
-	delay_file = h5py.File('bold_' + str(delay) + 's_delay.h5', 'w')
-	delay_file.create_dataset('responses', data=delay_bold)
-	delay_file.close()
+	# Save as BOLD with different delays (from 4-8 secs)
+	for delay in [4, 5, 6, 7, 8]:
+		delay_bold = np.full(bold.shape, np.nan, dtype = 'float32')
+		delay_bold[delay:, :] = bold[:-delay, :]
+		delay_file = h5py.File('bold_' + str(delay) + 's_delay.h5', 'w')
+		delay_file.create_dataset('responses', data=delay_bold)
+		delay_file.close()
 
-# Save as deconv BOLD
-deconv_bold = deconvolve_bold(bold) # deconvolve via polynomial division
-deconv_file = h5py.File('pd_deconv.h5', 'w')
-deconv_file.create_dataset('responses', data=deconv_bold)
-deconv_file.close()
+	# Save as deconv BOLD
+	deconv_bold = deconvolve_bold(bold) # deconvolve via polynomial division
+	deconv_file = h5py.File('pd_deconv.h5', 'w')
+	deconv_file.create_dataset('responses', data=deconv_bold)
+	deconv_file.close()
 
-# Close stimuli file
-voxel_file.close()
+	# Close stimuli file
+	voxel_file.close()
+
 
 def deconvolve_bold(bold):
 	""" Deconvolving the neural response via polydiv using the canonical HRF.
@@ -107,3 +110,7 @@ def deconvolve_bold(bold):
 	deconv_bold = (deconv_bold - deconv_mean)/deconv_std
 
 	return deconv_bold
+	
+	
+if __name__ == "__main__":
+	main()
