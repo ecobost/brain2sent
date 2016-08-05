fa587fafbb46a6a71e9a524060671aa1ca8ca905
ecobost <ecobos21@hotmail.com>
2016-08-04 22:50:59 -0500

    Rewrote generate feats and responses to save the feats and responses needed

--- code/generate_feats.py
+++ code/generate_feats.py
@@ -1,14 +1,12 @@
 # Written by: Erick Cobos T
-# Date: 20-July-2016
-""" Generates feature representations for every second of video using the image
-vectors.
+# Date: 04-August-2016
+""" Generates feature representations for every second of video using the video
+frames' image vectors.
 
-The first representation just subsamples and takes the image vector of the frame
-at the given timestep, the second averages over all feature representations in
-the previous second and the third convolves the features along the time axis 
-with a canonical HRF.
+The first representation averages over all frames in the previous second of 
+video and the second convolves the features along the time axis with a canonical
+HRF.
 """
-
 import h5py
 import numpy as np
 import nipy.modalities.fmri.hemodynamic_models as hm
@@ -22,19 +20,11 @@ full_feats = full_feats_file['feats']
 full_feats = np.array(full_feats)
 full_feats_file.close()
 
-# Subsample one every 15 rows to create feats
-feats = full_feats[14::15, :]
-
-# Save feats
-feats_file = h5py.File(file_prefix + 'feats.h5', 'w')
-feats_file.create_dataset('feats', data=feats)
-feats_file.close()
-
-# Average every 15 rows to create 1sec_feats
+# Average every 15 rows to create feats
 onesec_feats = full_feats.reshape(-1, 15, 768).mean(axis=1)
 
 # Save 1 second feats
-onesec_feats_file = h5py.File(file_prefix + '1sec_feats.h5', 'w')
+onesec_feats_file = h5py.File(file_prefix + 'feats.h5', 'w')
 onesec_feats_file.create_dataset('feats', data=onesec_feats)
 onesec_feats_file.close()
 
@@ -48,3 +38,5 @@ conv_feats = full_conv_feats[14:-479:15, :] # delete end and subsample
 conv_feats_file = h5py.File(file_prefix + 'conv_feats.h5', 'w')
 conv_feats_file.create_dataset('feats', data=conv_feats)
 conv_feats_file.close()
+
+print('Done!')
--- code/generate_responses.py
+++ code/generate_responses.py
@@ -1,123 +1,96 @@
 # Written by: Erick Cobos T (a01184587@itesm.mx)
-# Date: 17-July-2016
-""" Generates different brain activation matrices (in h5 files) from BOLD data.
+# Date: 04-August-2016
+""" Extract BOLD and ROI info and save as h5 files.
 
 Reads matrices from Gallant's data, creates the brain mask, drops invalid
-columns and generates a matrix (timesteps x voxels) for (1) BOLD activations, 
-(2) BOLD activations at different time delays (shifted by 4, 5, 6, 7 and 8 secs)
-and (3) estimated neural responses deconvolved via polynomial division. 
-See code for details.
+columns and generates a matrix (timesteps x voxels) for (1) original BOLD
+activations, (2) BOLD activations at different delays (4,5,6 and 7 secs) and
+(3) their temporally smoothed versions (1-d gaussian filter with sigma = 1.5 
+seconds). It also saves the ROI mask.
 
 Works for train or test responses.
 """
 import h5py
 import numpy as np
+from scipy import ndimage
+
+# Set params
+rt_or_rv = 'rt' # change to rv for test responses
+file_prefix = 'train_' # whether to write train or test files
+
+# Read BOLD data file
+voxel_file = h5py.File('VoxelResponses_subject1.mat', 'r')
+rois = voxel_file['roi']
+bold = voxel_file[rt_or_rv]
+
+# Extract ROIs
+brain = np.zeros([18, 64, 64])
+roi_id = 1
+for roi in rois:
+	brain = brain + roi_id*np.array(rois[roi])
+	print(roi, roi_id)
+	roi_id += 1
+	
+# Extract BOLD
+bold = np.array(bold).transpose()
+
+# Select voxels from all regions
+roi_mask = (brain > 0).flatten()
+bold = bold[:, roi_mask]
+
+# Clean data (delete columns with any nans)
+nan_cols = np.isnan(bold).any(axis=0)
+nan_mask = np.logical_not(nan_cols) 
+bold = bold[:, nan_mask] # 8982 remaining voxels for S1
+# Tested: Same voxels are dropped for train and test responses
+
+# Save as BOLD
+bold_file = h5py.File(file_prefix + 'bold.h5', 'w')
+bold_file.create_dataset('responses', data=bold)
+bold_file.close()
+
+# Create smooth BOLD
+smooth_bold = ndimage.gaussian_filter1d(bold, sigma=1.5, axis=0)
+
+# Save as smooth BOLD
+smooth_bold_file = h5py.File(file_prefix + 'smooth_bold.h5', 'w')
+smooth_bold_file.create_dataset('responses', data=smooth_bold)
+smooth_bold_file.close()
+
+# Save as BOLD with different delays (from 4-7 secs)
+for delay in [4, 5, 6, 7]:
+	delay_bold = np.full(bold.shape, np.nan, dtype='float32')
+	delay_bold[:-delay, :] = bold[delay:, :]
+	delay_file = h5py.File(file_prefix + str(delay) + 'sec_bold.h5', 'w')
+	delay_file.create_dataset('responses', data=delay_bold)
+	delay_file.close()
+	
+# Save as smooth BOLD with different delays (from 4-7 secs)
+for delay in [4, 5, 6, 7]:
+	delay_bold = np.full(smooth_bold.shape, np.nan, dtype='float32')
+	delay_bold[:-delay, :] = smooth_bold[delay:, :]
+	delay_file = h5py.File(file_prefix + str(delay) + 'sec_smooth_bold.h5', 'w')
+	delay_file.create_dataset('responses', data=delay_bold)
+	delay_file.close()
+
+# Create ROI per voxel (later used for voxel selection)
+roi_info = (brain[brain > 0])[nan_mask]
+
+# Save ROI info
+roi_file = h5py.File('roi_info.h5', 'w')
+roi_file.create_dataset('rois', data=roi_info)
+roi_file.close()
+
+# Close voxel file
+voxel_file.close()
+
+print('Done!')
+
+
+
+### Optionally,
 import matplotlib.pyplot as plt
 
-def main():
-	"""Main function. Executed if run as script."""
-	# Read BOLD data file
-	voxel_file = h5py.File('VoxelResponses_subject1.mat', 'r')
-	rois = voxel_file['roi']
-	bold = voxel_file['rt'] # 'rv' for test responses
-
-	# Extract ROIs (later used as mask)
-	brain = np.zeros([18, 64, 64])
-	i = 1
-	for roi in rois:
-		brain = brain + 8*i*np.array(rois[roi])
-		i += 1
-
-	# Save ROIs as images (to check against mean EPI and visualization)
-	for i in range(18):
-		plt.imsave('coronal_' + str(i+1) + '.png', brain[i,...])
-
-	# Create mean EPI
-	bold = np.array(bold).transpose()
-	epi_mean = bold.mean(axis = 0)
-	epi_mean = epi_mean.reshape([18, 64, 64])
-	for i in range(18):
-		plt.imsave('mean_' + str(i+1) + '.png', epi_mean[i,...])
-
-	# Select voxels from all regions
-	roi_mask = (brain > 0).flatten()
-	bold = bold[:, roi_mask]
-
-	# Clean data (delete columns with any nans)
-	nan_cols = np.isnan(bold).any(axis=0)
-	nan_mask = np.logical_not(nan_cols) 
-	bold = bold[:, nan_mask] # 8982 remaining voxels for S1
-	# Tested: Same voxels are dropped for test responses, i.e., masks are the same
-	
-	# Save voxels' ROI (for voxel selection)
-	roi_info = (brain[brain > 0])[nan_mask]
-	roi_file = h5py.File('roi_info.h5', 'w')
-	roi_file.create_dataset('rois', data=roi_info)
-	roi_file.close()
-	
-	# Save as BOLD
-	bold_file = h5py.File('bold.h5', 'w')
-	bold_file.create_dataset('responses', data=bold)
-	bold_file.close()
-
-	# Save as BOLD with different delays (from 4-8 secs)
-	for delay in [4, 5, 6, 7, 8]:
-		delay_bold = np.full(bold.shape, np.nan, dtype='float32')
-		delay_bold[delay:, :] = bold[:-delay, :]
-		delay_file = h5py.File('bold_' + str(delay) + 's_delay.h5', 'w')
-		delay_file.create_dataset('responses', data=delay_bold)
-		delay_file.close()
-
-	# Save as deconv BOLD
-	deconv_bold = deconvolve_bold(bold) # deconvolve via polynomial division
-	deconv_file = h5py.File('pd_deconv.h5', 'w')
-	deconv_file.create_dataset('responses', data=deconv_bold)
-	deconv_file.close()
-
-	# Close stimuli file
-	voxel_file.close()
-
-
-def deconvolve_bold(bold):
-	""" Deconvolving the neural response via polydiv using the canonical HRF.
-
-	"If u and v are vectors of polynomial coefficients, convolving them is
-	equivalent to multiplying the two polynomials, and deconvolution is 
-	polynomial division." (Explanation from Matlab's deconv function)
-
-	This is probably the simplest approach to deconvolution. See Wu et al., 2013
-	and Pedregosa et al., 2015 for better ways to do it: they estimate an HRF
-	and neural responses per voxel via GLM (both provide code).
-
-	Args:
-		bold: A matrix (timesteps x voxels). BOLD activations.
-
-	Returns:
-		deconv_bold: A matrix (timesteps x voxels). The (normalized) estimated 
-			neural responses that produced the input activations.
-	"""
-	import nipy.modalities.fmri.hemodynamic_models as hm
-	
-	deconv_bold = np.zeros_like(bold) # output matrix
-	hrf = hm.glover_hrf(1, oversampling=1) # 32-second canonical HRF
-	for voxel in range(bold.shape[1]):
-		# pad input by filter_size-1 to recover the entire response vector 
-		padded_bold = np.pad(bold[:, voxel], (0, 31), 'constant', 
-							 constant_values=0)
-	
-		# drop leading zeros in HRF and deconvolve
-		response, _ = np.polydiv(padded_bold, hrf[4:]) 
-	
-		# realign and store neural responses
-		deconv_bold[:, voxel] = response[4:]
-
-	# normalize per voxel
-	deconv_mean = deconv_bold.mean(axis=0)
-	deconv_std = deconv_bold.std(axis=0)
-	deconv_bold = (deconv_bold - deconv_mean)/deconv_std
-
-	return deconv_bold
-	
-	
-if __name__ == "__main__":
-	main()
+# Save ROIs as images (for visualization)
+for i in range(18):
+	plt.imsave('coronal_' + str(i+1) + '.png', brain[i,...])
