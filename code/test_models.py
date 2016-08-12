bf126f398ed711bd6bd254f8ed050b7a4140138f
ecobost <ecobos21@hotmail.com>
2016-08-12 04:25:59 -0500

    Skeleton of test models script. Yet to write

---
+++ code/test_models.py
@@ -0,0 +1,22 @@
+# Written by: Erick Cobos T
+# Date:
+
+#TODO: Wrote the script to test all models once trained, should generate all metrics
+
+def main:
+	"""Main function. Tests all models."""
+	test_linear('name of model', name of test set, name of feats)
+
+def test_linear(model_filename, features_filename, targets_filename):
+	print('Model', model_filename[:-3])
+	
+
+	# Load model
+	# Load test set
+	# Make predictions on test set
+	# Compute metrics (use scikit-learn metrics?)
+	# Print metrics to screen
+def test_neural_network():
+
+if name == "__main__":
+	main()
